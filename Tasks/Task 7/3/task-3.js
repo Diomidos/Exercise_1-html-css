cdd31f2bfa95174875a7eb9e221d0f26df7eb918
Diomidos <privozin@yandex.ru>
2022-11-20 19:14:05 +0300

    Create task-3.js

---
+++ Tasks/Task 7/3/task-3.js
@@ -0,0 +1,8 @@
+console.log(typeof console.log);
+console.log(typeof { name: 'Maxim' });
+console.log(typeof(Symbol('key')));
+console.log(typeof(Number));
+console.log(typeof(''));
+console.log(typeof(0));
+console.log(typeof(-10));
+console.log(typeof('-105'));
