e0a393853e0f273be99b8e2616f60561188803dc
Diomidos <privozin@yandex.ru>
2022-11-20 14:31:36 +0300

    Create data-types.js

---
+++ Tasks/Task 6/1/data-types.js
@@ -0,0 +1,21 @@
+const name = 'Diomid';
+console.log('name', name, typeof name);
+const age = 32;
+console.log('age', age, typeof age);
+const IamAnAstronaut = false;
+console.log('IamAnAstronaut', Boolean, typeof IamAnAstronaut);
+const jsDeveloper = null;
+console.log('jsDeveloper', null, typeof jsDeveloper);
+const fire = undefined;
+console.log('fire', undefined, typeof fire);
+const Im = {
+    name: 'Diomid',
+    age: 32,
+    IamAnAstronaut: false,
+}
+console.log('Im', Im, typeof Im);
+const id = Symbol('42');
+console.log('id', id, typeof id);
+const bigIntNumber = 1000n;
+console.log('bigIntNumber', bigIntNumber, typeof bigIntNumber);
+
