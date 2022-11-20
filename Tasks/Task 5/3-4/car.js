5c53c963634668e26b752f6597fa83c8c80f13f8
Diomidos <privozin@yandex.ru>
2022-11-20 11:15:55 +0300

    Create car.js

---
+++ Tasks/Task 5/3-4/car.js
@@ -0,0 +1,18 @@
+const carName = 'Jeep';
+let maxCarSpeed = '180';
+let carOwner = 'Diomid';
+alert(carName);
+alert(maxCarSpeed);
+alert(carOwner);
+maxCarSpeed = '170';
+carOwner = 'Olga';
+console.log('Скорость авто изменилась на', maxCarSpeed);
+console.log('Сменился владелец', carOwner);
+maxCarSpeed = '160';
+carOwner = 'Gleb';
+console.log('Скорость авто изменилась на', maxCarSpeed);
+console.log('Сменился владелец', carOwner);
+maxCarSpeed = '150';
+carOwner = 'Evgenia';
+console.log('Скорость авто изменилась на', maxCarSpeed);
+console.log('Сменился владелец', carOwner);
