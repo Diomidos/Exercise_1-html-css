973eb00a7a420fb6ce10005ecd0b0943eef54d4c
Diomidos <privozin@yandex.ru>
2022-11-22 01:33:14 +0300

    Create task-1.js

---
+++ Tasks/Task 8/1/task-1.js
@@ -0,0 +1,14 @@
+const header = 'Всем привет! Меня зовут';
+const myName = 'Diomid.';
+const text1 = 'Сейчас я изучаю язык программирования';
+const programmingLanguage = 'JavaScript';
+const text2 = 'на курсе по';
+const text3 = 'у';
+const courseCreatorName = 'Владилена Минина.';
+const text4 = 'Я хочу стать веб-разработчиком, потому что';
+const reasonText = 'мне это нравится.';
+const text5 = 'До этого я изучал';
+const numberOfMonth = '1 месяц.';
+const text6 = 'Я уверен, что пройду данный курс до конца!';
+const allInfo = `${header} ${myName} ${text1} ${programmingLanguage} ${text2} ${programmingLanguage} ${text3} ${courseCreatorName} ${text4} ${reasonText} ${text5} ${programmingLanguage} ${numberOfMonth} ${text6}`;
+console.log(allInfo);
