a4fdcd58f17071daa780b277a9474cdf27cd7d3a
Diomidos <privozin@yandex.ru>
2022-11-22 01:34:04 +0300

    Create task-2.js

---
+++ Tasks/Task 8/2/task-2.js
@@ -0,0 +1,7 @@
+let myInfoText = 'Всем привет! Меня зовут Diomid. Сейчас я изучаю язык программирования JavaScript на курсе по JavaScript у Владилена Минина. Я хочу стать веб-разработчиком, потому что мне это нравится. До этого я изучал JavaScript 1 месяц. Я уверен, что пройду данный курс до конца!';
+  console.log(myInfoText.replaceAll('JavaScript', 'javascript'));
+  console.log(myInfoText.replaceAll('курс', 'КУРС'));
+  console.log(myInfoText.length);
+  console.log(myInfoText[0]);
+  console.log(myInfoText[264]);
+  
