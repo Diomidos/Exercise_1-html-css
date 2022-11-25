196c298492de4cf3b3536b3c9df1cc96fa6e5084
Diomidos <privozin@yandex.ru>
2022-11-26 00:30:35 +0300

    Create task-3.js

---
+++ Tasks/Task 9/3/task-3.js
@@ -0,0 +1,20 @@
+let question_1 = confirm('JavaScript появился в 1995 году?');
+if (question_1) {
+    alert('Верно!');
+} else {
+    alert('1995');
+}
+let question_2 = confirm('Спецификация JavaScript называется ECMAScript?');
+if (question_2) {
+    alert('Верно!');
+} else {
+    alert('ECMAScript');
+}
+let question_3 =  confirm('JavaScript был создан за 1 месяц?');
+if(question_3) {
+    alert('Верно!');
+} else {
+    alert('1 месяц');
+}
+
+
