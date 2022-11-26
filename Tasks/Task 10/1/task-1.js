b9c004fff919ef863c4ebbe2675254ecd0c91a5b
Diomidos <privozin@yandex.ru>
2022-11-26 14:32:36 +0300

    Create task-1.js

---
+++ Tasks/Task 10/1/task-1.js
@@ -0,0 +1,11 @@
+let health = prompt('Введите число параметра "здоровье" для персонажа')
+if (health) {
+    debugger
+  health < 0 || health;
+  debugger
+  alert('Параметр "здоровье" должен быть больше нуля!')
+} else  {
+  alert(`'Параметр здоровье равен', ${Health}`);
+  debugger
+}
+console.log(health);
