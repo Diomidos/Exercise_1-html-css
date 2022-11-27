b62f63c04cdb131d602183e8808fafda2a2398c2
Diomidos <privozin@yandex.ru>
2022-11-27 16:17:59 +0300

    Create task-1.js

---
+++ Tasks/Task 11/1/task-1.js
@@ -0,0 +1,22 @@
+name();
+function name() {
+    return name;
+};
+console.log('Diomid');
+
+
+
+function name() {
+    return name;
+};
+console.log('Olga');
+name();
+
+
+
+const myName = (name) => {
+    return name;
+};
+const result = myName('Gleb');
+console.log(result);
+
