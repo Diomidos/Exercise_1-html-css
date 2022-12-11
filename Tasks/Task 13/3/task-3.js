652f0ff78bf7264f85c44b7a1ed1e28386db6961
Diomidos <privozin@yandex.ru>
2022-12-11 18:36:16 +0300

    Update task-3.js

--- Tasks/Task 13/3/task-3.js
+++ Tasks/Task 13/3/task-3.js
@@ -1,58 +1,14 @@
-// delete - если action равен delete, то функция handleObject() должна удалить свойство key из объекта obj и возвратить обновленный объект.
-
-// function handleObject(obj, key, action) {
-//     console.log(action) 
-//         if (action === 'delete') { //если тип данных action = delete
-//             delete obj[key];  // key равен 'programmingLanguage' удаляем этот ключ
-//         } else {
-//             console.log(obj); // если ключа нет, вернуть итоговый объект
-//         }
-//        };
-    
-//        const student = {
-//         name: 'Maxim',
-//         programmingLanguage: 'JavaScript',
-//      };
-    
-//        handleObject(student, 'programmingLanguage', 'delete');
-//        handleObject(student, 'programmingLanguage', 'add');
-       
-
-// get - если action равен get, то функция handleObject() должна вернуть значение ключа key в объекте obj.
-
-    //    function handleObject(obj, key, action) {
-    //     console.log(action) 
-    //         if (action === 'get') { 
-    //             delete obj[key];  
-    //         } else {
-    //             console.log(obj); 
-    //         }
-    //        };
-        
-    //        const student = {
-    //         name: 'Maxim',
-    //         programmingLanguage: 'JavaScript',
-    //      };
-        
-    //        handleObject(student, 'programmingLanguage', 'get');
-    //        handleObject(student, 'programmingLanguage', 'add');
-
-
-    // add - если action равен add, то функция handleObject() должна добавить новый ключ key в объект object и присвоить значение пустой строки "". Также из функции необходимо возвратить обновленный объект obj.
-
-           function handleObject(obj, key, action) {
-            console.log(action) 
-                if (action === 'add') { 
-                    delete obj[key];  
-                } else {
-                    console.log(obj); 
-                }
-               };
-            
-               const student = {
-                name: 'Maxim',
-                programmingLanguage: 'JavaScript',
-             };
-            
-               handleObject(student, 'programmingLanguage', 'add');
-               handleObject(student, 'programmingLanguage', 'add');
+function handleObject(obj, key, action) {
+    console.log(action) 
+if (action === 'add') { 
+    delete obj[key];  
+} else {
+    console.log(obj); 
+}
+};
+const student = {
+    name: 'Maxim',
+    programmingLanguage: 'JavaScript',
+};         
+handleObject(student, 'programmingLanguage', 'add');
+handleObject(student, 'programmingLanguage', 'add');
